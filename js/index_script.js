7abc5263c0ad2baab4d2ca35b13eab345d8bd3f3
OlgaRacu <olgaracu1983@gmail.com>
2016-10-06 16:40:57 -0400

    categoryID

--- js/index_script.js
+++ js/index_script.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    var categoryID = $('.categoryIdClass').text();
-    alert(categoryID);
+
+    //alert(categoryID);
     $('#index-products').load('/category/' + 1);
 
     $("#navCategory").on("click", "button", function () {
 
-       /* var array_categoryID = ($('#category_ID').val()).split('_');
-        var categorID = array_categoryID[0];
-alert(categoryID);*/
+        var category_ID = $(this).attr('id');
+        //alert(categoryID);
 
-
+        var array_categoryID = category_ID.split('_');
+        var categoryID = array_categoryID[1];
+        alert(categoryID);
 
         /* var result = ($(this).text());
          * categoryString = result.toString().toLowerCase()
@@ -19,7 +20,7 @@ alert(categoryID);*/
          .replace(/^-+/, '')             // Trim - from start of text
          .replace(/-+$/, '');            // Trim - from end of text
          alert(categoryString);*/
-        alert(categoryID);
+
         $('#index-products').load('/category/' + categoryID);
     });
 
